a6ccf11cd8023a2eb1a34456f30722436cf17ac8
blindlq <735560449@qq.com>
2023-06-08 20:27:56 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt/>
+            </div>
+            <el-form ref="loginFormRef" :model="loginForm" class="login_form" label-width="0">
+                <el-form-item >
+                    <el-input v-model="loginForm.username"></el-input>
+                </el-form-item>
+                <el-form-item >
+                    <el-input v-model="loginForm.password"></el-input>
+                </el-form-item>
+                <el-form-item  class="btns">
+                    <el-button type="primary">提交</el-button>
+                    <el-button type="info">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                loginForm: {
+                    username: "username",
+                    password: "password"
+                }
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+</style>
